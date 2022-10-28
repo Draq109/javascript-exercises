85c29bcb68b223278b276ffa3d6058a7aac1211a
Draq109 <daniel.vazquez03@utrgv.edu>
2022-10-28 09:59:15 -0500

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //if (/^[a-zA-Z0-9\s.,!?]+$/.test(string))
+    // if statement will check only for letters,numbers,periods,commas, etc.
+    let reverse = '';
+    for (let i = string.length - 1; i >= 0; i--) 
+        reverse += string[i];
+    return reverse
 };
+
+console.log(reverseString("Hello World."));
+
 
 // Do not edit below this line
 module.exports = reverseString;
