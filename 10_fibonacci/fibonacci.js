65ab54011318f138bf781924df43bd9fb7758093
Draq109 <daniel.vazquez03@utrgv.edu>
2022-10-30 15:32:48 -0600

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num === 1 || num === 2)
+        return 1;
+    if (num < 0)
+        return "OOPS";
+    let fib = [1,1];
+    let prevVal, nextVal = 1;
+    for (let i = 0; i < num; i++) {
+        prevVal = fib[i];
+        nextVal += prevVal;
+        fib.push(nextVal);
+    }
+    return fib[num - 1];
 };
 
 // Do not edit below this line
