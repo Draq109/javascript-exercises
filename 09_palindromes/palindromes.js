f02b9e406c2a68a339dcbae746a543924c6e2f61
Draq109 <daniel.vazquez03@utrgv.edu>
2022-10-30 15:13:46 -0600

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let stripped = string.replace(/[\s.,!?]/g, '');
+    stripped = stripped.toLowerCase();
+    let j = stripped.length - 1;
+    for (let i = 0; i < stripped.length / 2; i++) {
+        if (stripped[i] === stripped[j]) {
+            j--;
+            continue;
+        }
+        else
+            return false;
+    }
+    return true;
 };
+console.log(palindromes('racecar'));
 
 // Do not edit below this line
 module.exports = palindromes;
