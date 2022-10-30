7ecd3bcefd16553220969308e15976b27df3014c
Draq109 <daniel.vazquez03@utrgv.edu>
2022-10-30 15:57:28 -0600

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    const sortedPeople = array.sort((a, b) => {
+        if (!('yearOfDeath' in a))
+            if (a.yearOfBirth < b.yearOfBirth)
+                return -1;
+            else
+                return 1;
 
+        if (!('yearOfDeath' in b))
+            if (b.yearOfBirth < a.yearOfBirth)
+                return 1;
+            else
+                return -1;
+
+
+        if (a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth)
+            return -1;
+        else
+            return 1;
+    })
+    return sortedPeople[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
